5c3366050a1282ef643d1119bb8b56c3a7520fc9
KgothatsoTheko <kgothatsotheko7@gmail.com>
2023-12-21 18:21:32 +0200

    javaScript continue pt1

--- index.js
+++ index.js
@@ -1,4 +1,64 @@
-const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=-26.11134&lon=28.16935&appid=0ce1aa27decb947120fb897abc655f72'
-const weather = []
+const location1 = document.querySelector('.location')
+const temperature = document.querySelector('.temperature-value ')
+const logo = document.querySelector('.logo')
+const notification = document.querySelector('.notification')
+const weather = {}
+
+weather.temperature = {
+    unit: 'Celsius'
+}
+
+setPosition = (position) => {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    getWeather(latitude, longitude);
+}
+
+showError = (error) => {
+    notification.style.display = 'block';
+    notification.innerHTML = `<p>${error.message} </p>`
+
+}
+
+getWeather = (latitude, longitude) => {
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=0ce1aa27decb947120fb897abc655f72`
+    
+    fetch(api)
+    .then(function(response) {
+        let data = response.json();
+        return data;
+    })
+    .then(function(data){
+        weather.temperature.value = Math.floor(data.main.temp - kelvin);
+        weather.city = data.name
+        weather.country = data.sys.country;
+        weather.logo = data.weather[0].icon
+    })
+    .then(function(){
+        displayWeather();
+    })
+
+}
+
+displayWeather = () => {
+    logo.innerHTML = `<img src = "icons/${weather.icon}.png"/>`;
+    temperature.innerHTML = `${weather.temperature.value}&deg<span>C</span>`
+    location1.innerHTML = `${weather.country}, ${weather.city}`;
+
+}
+
+const kelvin = 273;
+
+if('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+} else {
+    notification.style.display = 'block';
+    notification.innerHTML = '<p>Browser does not support Geolocation</p>';
+}
+
+
+
+
 
 
